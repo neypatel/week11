96b92724840b24d9f0e0a5fe0164a4da5e3649ba
Ney Patel <76930595+neypatel@users.noreply.github.com>
2024-03-11 16:12:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-
+/*
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
@@ -76,7 +76,7 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
+*/
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
